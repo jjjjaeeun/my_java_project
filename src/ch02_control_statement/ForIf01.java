23dbd908311e5cc206c113107b875c853d6e9bc5
jjy <jjy@naver.com>
2025-07-28 17:25:08 +0900

    25.7.28 오후 연습

---
+++ src/ch02_control_statement/ForIf01.java
@@ -0,0 +1,18 @@
+package ch02_control_statement;
+
+public class ForIf01 {
+    public static void main(String[] args) {
+        int sum0=0,sum1=0,sum2=0;
+
+        for (int i = 1; i <11 ; i++) {
+
+            if (i % 3 == 00) {sum0 +=i;
+            }else if (i%3==1){sum1 +=i;
+            }else if (i%3==2){sum2 +=i;
+            }
+        }
+        System.out.println("sum0: "+sum0);
+        System.out.println("sum1: "+sum1);
+        System.out.println("sum2: "+sum2);
+    }
+}
