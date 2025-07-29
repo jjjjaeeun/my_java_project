2b39a66925bb2e8e06b20846063ecb0e87479226
jjy <jjy@naver.com>
2025-07-29 18:07:22 +0900

    25.7.29 오후 연습

---
+++ src/ch03_array/Array02.java
@@ -0,0 +1,16 @@
+package ch03_array;
+
+public class Array02 {
+    public static void main(String[] args) {
+        String bts []={"진","뷔","정국","rm","지민","슈가","제이홉"};
+        for (int i = 0; i < bts.length; i++) {
+            //System.out.println("bts["+i+"]"+"="+bts[i]);
+
+            if(i !=6){
+                System.out.print(bts[i]+", ");
+            }else{ System.out.print(bts[i]);
+            }
+
+        }
+    }
+}
